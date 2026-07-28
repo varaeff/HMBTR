869500b735d8dd4c8e3c0a8ecd1834f48cdbd04a
varaeff <varaev.sergey@gmail.com>
2026-07-28 13:34:22 +0400

    fix: fixing prod error

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 echo "Applying database schema migrations..."
-npx prisma db push --schema ./prisma/schema.prisma
+npx prisma db push --schema ./prisma/schema.prisma --accept-data-loss
 
 echo "Starting HMBTR backend..."
 exec "$@"
